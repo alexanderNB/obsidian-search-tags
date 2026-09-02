c243b1c50b929b03d5cb1f1d282f6229e1085a4f
alexanderNB <alexander@bjerking.dk>
2026-09-02 13:03:04 +0200

    Pretty much good

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new MainSuggestModal(this.app).open();
+						new MainSuggestModal(this).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -71,14 +71,68 @@ export default class MyPlugin extends Plugin {
 
 
 class MainSuggestModal extends FuzzySuggestModal<TFile | string> {
+
 	choosenTags: string[] = [];
 
+
+    constructor(plugin: MyPlugin) {
+        super(plugin.app)
+        this.scope.register(["Shift"], 'Enter', e => {
+            e.preventDefault();
+            let newFile = this.app.vault.create(this.inputEl.value + ".md", "");
+            newFile.then((file) => this.app.workspace.getLeaf().openFile(file));
+            this.close();
+        })
+
+        this.scope.register(["Ctrl", "Shift"], 'End', e => { // TODO: Probably remove this and fix keyboard
+            e.preventDefault();
+            let date = new Date()
+            let newFile = this.app.vault.create(
+                this.inputEl.value + ".md",
+                "---\n" +
+                "Dato: " + date.getFullYear() + "-" + (date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()) + "-" + (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + "\n" +
+                "tags:\n" +
+                this.choosenTags.map((tag: string) => "  - " + tag.slice(1) + "\n").join("") +
+                "---\n" +
+                "# " + this.inputEl.value + "\n"
+            );
+            newFile.then((file) => this.app.workspace.getLeaf().openFile(file));
+            this.close();
+        })
+
+        this.scope.register(["Ctrl", "Shift"], 'Enter', e => {
+            e.preventDefault();
+            let date = new Date()
+            let newFile = this.app.vault.create(
+                this.inputEl.value + ".md",
+                "---\n" +
+                "Dato: " + date.getFullYear() + "-" + (date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()) + "-" + (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + "\n" +
+                "tags:\n" +
+                this.choosenTags.map((tag: string) => "  - " + tag.slice(1) + "\n").join("") +
+                "---\n" +
+                "# " + this.inputEl.value + "\n"
+            );
+            newFile.then((file) => this.app.workspace.getLeaf().openFile(file));
+            this.close();
+        })
+
+        this.scope.register([], 'Tab', e => {
+            e.preventDefault();
+            for(let i = 0; i < this.resultContainerEl.children.length; i++) {
+                if (this.resultContainerEl.children.item(i)?.classList.contains("is-selected")) {
+                    this.inputEl.value = this.resultContainerEl.children.item(i)?.getText() || ""
+                    break
+                }
+            }
+        })
+    }
+
     getItemText(item: TFile | string): string {
-		if (typeof item == "string") {
-			return item;
-		} else {
-			return item.name;
-		}
+        if (typeof item == "string") {
+            return item;
+        } else {
+            return item.name;
+        }
     }
 	
     getItems(): (TFile | string)[] {
@@ -123,21 +177,23 @@ class MainSuggestModal extends FuzzySuggestModal<TFile | string> {
 		})
 		let combinedArray: (TFile | string)[] = files;
 		tags.forEach((tag: string) => combinedArray.push(tag))
-		console.log(combinedArray)
+		// console.log(combinedArray)
 		return combinedArray;
     }
 
 	renderSuggestion(match: FuzzyMatch<(TFile | string)>, el: HTMLElement) {
 		const titleEl = el.createDiv();
 		if (match.item instanceof TFile) {
-			console.log(match.item.name)
 			if (!this.inputEl.value.startsWith("#")) {
-				renderResults(titleEl, match.item.name, match.match);
+                if (match.item.name.endsWith(".md")) {
+                    renderResults(titleEl, match.item.name.slice(0, -3), match.match);
+                } else {
+                    renderResults(titleEl, match.item.name, match.match);
+                }
 			} else {
 				el.remove()
 			}
 		} else {
-			console.log(match.item)
 			if (this.inputEl.value.startsWith("#")) {
 				if (this.choosenTags.contains(match.item)) {
 					renderResults(titleEl, "\uf00d " + match.item, match.match);
@@ -170,4 +226,6 @@ class MainSuggestModal extends FuzzySuggestModal<TFile | string> {
 			this.open();
 		}
     }
+
+
 }
